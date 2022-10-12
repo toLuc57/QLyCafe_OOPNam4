95df0f33f7cb6ef9a79d71b1a06eb134c6d56941
lucTo57 <nhocphan0@gmail.com>
2022-10-12 21:09:14 +0700

    By HM

--- giuaKy/src/Util/dbUtil.java
+++ giuaKy/src/Util/dbUtil.java
@@ -8,8 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -19,40 +18,44 @@ import javax.swing.JOptionPane;
 public class dbUtil {
     public static Connection conn = null;
     public static Connection getConnection() {
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=TheCoffee";
+        String user = "sa";
+        String password ="1234";
         try {
-            String hostName = "localhost";
-            String sqlInstanceName = "SQLEXPRESS";
-            String database = "TheCoffee";
-            String userName = "sa";
-            String password = "1234";
-            
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            // Cấu trúc URL Connection dành cho SQLServer
-            // Ví dụ:
-            // jdbc:sqlserver://ServerIp:1433/SQLEXPRESS;databaseName=Coffee
-            String connectionURL = "jdbc:sqlserver://" + hostName + ":1433"
-                    + ";instance=" + sqlInstanceName + ";databaseName=" + database;
-            
-            conn = DriverManager.getConnection(connectionURL, userName,
-                    password);
-            return conn;
-        } catch (SQLException | ClassNotFoundException ex) {
-            Logger.getLogger(dbUtil.class.getName()).log(Level.SEVERE, null, ex);
+            conn = DriverManager.getConnection(url,user,password);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return null;
+        return conn;
 }
-    
-public ArrayList<ThucDon> GetThucDon(){
+    public static ResultSet ThucThiSelect(String sql ) throws SQLException{
+        Statement st;
+        try {
+            st = conn.createStatement();
+            System.out.println("Select thanh cong");
+            return st.executeQuery(sql);               
+        } catch (SQLException ex) {
+            System.out.println("Loi thuc thi lenh");
+        }
+        conn.close();
+        return null;
+    }
+     
+public ArrayList<ThucDon> GetThucDon(String txt, String loai){
         ArrayList<ThucDon> arrThucDon = null;
         String sql;
- 
-            sql = "Select * From Product";
+        sql = "Select * From ThucDon where TenThucDon like '%"+txt+"%'";
+        if(!loai.equals("All")){
+            String phanloai = " and Loai = N'"+loai+"'";
+            sql += phanloai;
+        }
         try{
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(sql);
             arrThucDon = new ArrayList<>();
             while(rs.next()){
-                ThucDon td = new ThucDon(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5));
+                ThucDon td = new ThucDon(rs.getString("IDThucDon"), rs.getString("TenThucDon"), rs.getString("Loai"), rs.getInt("GiaTien"), rs.getString("HinhAnh"));
                 arrThucDon.add(td);
             }
         }catch(SQLException ex){
@@ -60,6 +63,26 @@ public ArrayList<ThucDon> GetThucDon(){
         }
         return arrThucDon;        
     }
+public List<String> GetLoaiThucDon(){
+     String Loai = "";
+     List<String> ls = new ArrayList<>();
+      Connection conn = dbUtil.getConnection();
+      String sql = "Select distinct Loai from ThucDon";
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            ls.add("All");
+            while(rs.next()){
+                ls.add(rs.getString("Loai"));
+            }
+           
+            
+        } catch (SQLException ex) {
+            System.out.println("Loi Get Loai Thuc Don");
+        }
+        return ls;
+  }
+
 public ArrayList<Ban> GetBan(){
         ArrayList<Ban> arrBan = null;
         String sql;
@@ -67,13 +90,13 @@ public ArrayList<Ban> GetBan(){
         try{
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            arrBan = new ArrayList<>();
+            arrBan = new ArrayList<Ban>();
             while(rs.next()){
-                Ban sp = new Ban(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getInt(4));
+                Ban sp = new Ban(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getInt(4));
                 arrBan.add(sp);
             }
-        }catch(SQLException ex){
-            JOptionPane.showMessageDialog(null, "Đã xảy ra lỗi !");
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Lỗi GetBan");
         }
         return arrBan; 
     } 
